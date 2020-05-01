6bc5c63eb0b9fd53ec95121e61b33abf1603544b
archist <hi.cyril@gmail.com>
2020-05-01 19:56:12 +0200

    Updates

---
+++ precache-manifest.860ce4db79346a8bef88cde89eedbfbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7edd452d1a09f7d85a62408fbcebc874",
+    "url": "/ocr_project4_react/index.html"
+  },
+  {
+    "revision": "01562727a7bc2c2a88c1",
+    "url": "/ocr_project4_react/static/css/main.a1837c7b.chunk.css"
+  },
+  {
+    "revision": "06e1f970d088bf205d2d",
+    "url": "/ocr_project4_react/static/js/2.0d8b536e.chunk.js"
+  },
+  {
+    "revision": "53185cd705dca0c7baf722c57c60f980",
+    "url": "/ocr_project4_react/static/js/2.0d8b536e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01562727a7bc2c2a88c1",
+    "url": "/ocr_project4_react/static/js/main.72b9faa5.chunk.js"
+  },
+  {
+    "revision": "85623bad6882fed8d1e9",
+    "url": "/ocr_project4_react/static/js/runtime-main.34c33ff1.js"
+  },
+  {
+    "revision": "cd8f62c3c0ef3cb3939875e2f81f40b5",
+    "url": "/ocr_project4_react/static/media/art.cd8f62c3.png"
+  }
+]);
